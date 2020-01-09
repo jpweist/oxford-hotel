7aea964d5479d7941af463314361ef2433dfc454
Jim Weist <jp_weist@yahoo.com>
2020-01-09 12:00:18 -0700

    Passed test find booking for any given day

--- src/Hotel.js
+++ src/Hotel.js
@@ -3,10 +3,13 @@ class Hotel {
     this.data = data;
   }
   findUserById(id) {
-  return this.data.users.filter(user => user.id === id)
+    return this.data.users.filter(user => user.id === id)
   }
   findUserByName(name) {
     return this.data.users.filter(user => user.name == name)
+  }
+  findBookingsByDay(day) {
+    return this.data.bookings.filter(booking => booking.date === day)
   }
 }
 
--- test/hotel-test.js
+++ test/hotel-test.js
@@ -35,7 +35,19 @@ describe('Hotel', function() {
     expect(hotel.findUserByName('Leatha Ullrich')).to.deep.equal([{id: 1, name: 'Leatha Ullrich'}])
   })
 
-  it.skip('Should find bookings for any given day', function() {
+  it('Should find bookings for any given day', function() {
+    hotel.findBookingsByDay("2019/11/20");
+    expect(hotel.findBookingsByDay("2019/11/20")).to.deep.equal([{
+      id: 1572293130170,
+      userID: 1,
+      date: "2019/11/20",
+      roomNumber: 1
+      },{
+      id: 1572293130171,
+      userID: 2,
+      date: "2019/11/20",
+      roomNumber: 2
+    }])
   })
 
   it.skip('Should find revenue for any given day', function() {
