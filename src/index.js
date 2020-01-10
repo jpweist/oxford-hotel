c18ca8952e1767f6ac0488c1c9dc86c0885c84d6
Jim Weist <jp_weist@yahoo.com>
2020-01-10 06:36:00 -0700

    Adjusted webpack to have mulitiple html files for customer and manager

--- src/index.js
+++ src/index.js
@@ -1,13 +1,27 @@
-// This is the JavaScript entry file - your code begins here
-// Do not delete or rename this file ********
 
-// An example of how you import jQuery
-//into a JS file if you use jQuery in that file
 import $ from 'jquery';
-
-// An example of how you tell webpack to use a
-//CSS (SCSS) file
 import './css/base.scss';
 
-// An example of how you tell webpack to use an
-//image (also need to link to it in the index.html)
+
+
+
+
+$('.login-button').on( "click", function() { // login manager or customer
+
+  let userName = $('.user-login').val();
+  let userpassword = $('.password').val();
+
+  if (userName === 'manager' && userpassword === 'overlook2019') {
+    // console.log(userName)
+
+    window.location = '/manager.html';
+  }
+   if (userName === 'customer' && userpassword === 'overlook2019') {
+     // let customer1 = new Customer(userName);
+     // console.log(userName)
+    window.location = '/customer.html'
+  }
+  if (userName !== 'manager' && userpassword !== 'overlook2019' || userName !== 'customer' && userpassword !== 'overlook2019') {
+    console.log('failed login')
+  }
+})
